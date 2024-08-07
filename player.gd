de6724b0b11bd5d35dbba0566e4faa59ba5603a2
Jacob Howell <jhowell45@googlemail.com>
2024-08-07 19:44:32 +0100

    Getting basic movement in place.

--- player.gd
+++ player.gd
@@ -1,5 +1,17 @@
 extends CharacterBody2D
 
+const SPEED = 600.0
+
+var direction = Vector2(0, 0)
 
 func _physics_process(delta):
-	pass
+	#var direction = Input.get_vector(null, null, "ui_up", "ui_down")
+	if Input.is_action_pressed("ui_up"):
+		direction.y = -1
+	if Input.is_action_pressed("ui_down"):
+		direction.y = 1
+
+	velocity = direction * SPEED
+	
+	move_and_slide()
+	direction = Vector2(0, 0)
