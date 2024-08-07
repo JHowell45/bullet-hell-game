c4d0c5e2d9e712c6dd93a09f0b8dd3593f4be7e8
Jacob Howell <jhowell45@googlemail.com>
2024-08-07 20:35:45 +0100

    basic player movement.

--- player.gd
+++ player.gd
@@ -1,23 +1,24 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-
+const SPEED = 500.0
 const ROTATE_DEGREES = 0.1
+const DECELERATION = 150
 
-var direction = Vector2(0, 0)
+var direction = Vector2.ZERO
 
 func _physics_process(delta):
 	#var direction = Input.get_vector(null, null, "ui_up", "ui_down")
 	if Input.is_action_pressed("ui_up"):
 		direction.y = -1
-	if Input.is_action_pressed("ui_down"):
-		direction.y = 1
+		velocity = (direction * SPEED).rotated(rotation)
 	if Input.is_action_pressed("ui_left"):
 		rotation -= ROTATE_DEGREES
+		#velocity = (direction * SPEED).rotated(rotation)
 	if Input.is_action_pressed("ui_right"):
 		rotation += ROTATE_DEGREES
-
-	velocity = (direction * SPEED).rotated(rotation)
-	
+		#velocity = (direction * SPEED).rotated(rotation)
+	velocity = velocity.move_toward(Vector2.ZERO, DECELERATION * delta)
+	print(velocity)
 	move_and_slide()
-	direction = Vector2(0, 0)
+	
+	#direction = Vector2.ZERO
