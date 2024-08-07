e9e9200ce3cb8deebdf8fc7c4c41cbcac968549e
Jacob Howell <jhowell45@googlemail.com>
2024-08-07 22:53:58 +0100

    Timer and random spawns for the asteroids.

---
+++ game.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func spawn_asteroid():
+	var asteroid = preload("res://asteroid.tscn").instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	asteroid.global_position = %PathFollow2D.global_position
+	add_child(asteroid)
+	
+
+
+func _on_timer_timeout():
+	spawn_asteroid()
