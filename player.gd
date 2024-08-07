b2eedcdfc05fd2737bd7906c4180196a7721a4a8
Jacob Howell <jhowell45@googlemail.com>
2024-08-07 21:22:40 +0100

    UPdated player script.

--- player.gd
+++ player.gd
@@ -7,18 +7,12 @@ const DECELERATION = 150
 var direction = Vector2.ZERO
 
 func _physics_process(delta):
-	#var direction = Input.get_vector(null, null, "ui_up", "ui_down")
 	if Input.is_action_pressed("ui_up"):
 		direction.y = -1
 		velocity = (direction * SPEED).rotated(rotation)
 	if Input.is_action_pressed("ui_left"):
 		rotation -= ROTATE_DEGREES
-		#velocity = (direction * SPEED).rotated(rotation)
 	if Input.is_action_pressed("ui_right"):
 		rotation += ROTATE_DEGREES
-		#velocity = (direction * SPEED).rotated(rotation)
 	velocity = velocity.move_toward(Vector2.ZERO, DECELERATION * delta)
-	print(velocity)
 	move_and_slide()
-	
-	#direction = Vector2.ZERO
