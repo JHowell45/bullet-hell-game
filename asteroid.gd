70119ab9fbbb2cae7a8bd233ae9a6a20b9aa6297
Jacob Howell <jhowell45@googlemail.com>
2024-08-07 23:04:16 +0100

    Give the asteroid an initial speed.

--- asteroid.gd
+++ asteroid.gd
@@ -1,16 +1,21 @@
 extends CharacterBody2D
 
-const DECELERATION = 30
+const DECELERATION = 5
 
 var fixed_rotation = randf_range(-0.1, 0.1)
+var speed = 300 * randf_range(0.5, 1.5)
+
+func _ready():
+	var player = get_node("/root/Game/Player")
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * speed
+	move_and_slide()
 
 func _physics_process(delta):
 	rotation += fixed_rotation
 	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
 	move_and_slide()
 	for i in get_slide_collision_count():
-		print("Sub Velocity: %s" % get_slide_collision(i).get_collider().velocity)
 		velocity += get_slide_collision(i).get_collider().velocity
 	velocity = velocity.move_toward(Vector2.ZERO, DECELERATION * delta)
-	print(velocity)
 	
