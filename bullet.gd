4f0b6094d797a4a7f51235e2c96b9eecd8e0e849
Jacob Howell <jhowell45@googlemail.com>
2024-08-09 00:03:24 +0100

    Updated distance position data to remove when out of sight. Also increase speed of bullet.

--- bullet.gd
+++ bullet.gd
@@ -1,16 +1,17 @@
 extends Area2D
 
-const SPEED = 1000
-const RANGE = 500
+const SPEED = 1500
+const RANGE = 1500
 
 var travelled_distance = 0
+
+@onready var player: CharacterBody2D = get_node("/root/Game/Player")
 
 func _physics_process(delta):
 	var direction = Vector2.UP.rotated(rotation)
 	position += direction * SPEED * delta
 	
-	travelled_distance += SPEED * delta
-	if travelled_distance > RANGE:
+	if player.global_position.distance_to(global_position) > RANGE:
 		queue_free()
 	
 	
@@ -18,3 +19,5 @@ func _physics_process(delta):
 
 func _on_body_entered(body):
 	queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage()
