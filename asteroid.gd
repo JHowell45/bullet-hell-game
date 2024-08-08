3484c4ca51cee5c83b38f46a079ac3f51bc06988
Jacob Howell <jhowell45@googlemail.com>
2024-08-08 20:29:18 +0100

    Updating asteroid physics.

--- asteroid.gd
+++ asteroid.gd
@@ -4,7 +4,7 @@ signal destroyed
 
 const DECELERATION = 5
 
-const MAX_DISTANCE = 100
+const MAX_DISTANCE = 1000
 
 var fixed_rotation = randf_range(-0.1, 0.1)
 var speed = 300 * randf_range(0.5, 1.5)
@@ -16,16 +16,20 @@ func _ready():
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * speed
 	move_and_slide()
+	
 
 func _physics_process(delta):
+	delete_if_too_far()
 	rotation += fixed_rotation
 	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
 	move_and_slide()
-	var response_velocity = Vector2.ZERO
-	for i in get_slide_collision_count():
-		response_velocity += get_slide_collision(i).get_collider().velocity
-	if !response_velocity.is_zero_approx():
-		velocity = response_velocity
+	if get_slide_collision_count() > 0:
+		print("%s || COLLISION: %s" % [get_rid(),  get_slide_collision_count()])
+		velocity += get_last_slide_collision().get_collider_velocity()
+	#for i in get_slide_collision_count():
+		#response_velocity += get_slide_collision(i).get_collider().velocity
+	#if !response_velocity.is_zero_approx():
+		#velocity = response_velocity
 	#if !response_velocity.is_zero_approx():
 		#if velocity.is_zero_approx():
 			#velocity = response_velocity
@@ -34,7 +38,7 @@ func _physics_process(delta):
 			#velocity.x *= -1
 			#velocity.y *= -1
 	#velocity = velocity.move_toward(Vector2.ZERO, DECELERATION * delta)
-	#print("%s: %s" % [get_rid(), velocity])
+	print("%s: %s" % [get_rid(), velocity])
 	
 
 func delete_if_too_far():
