d7e77f01021c084d87e4d1682da7513169302206
Jacob Howell <jhowell45@googlemail.com>
2024-08-08 23:42:26 +0100

    Updated the game score properly

--- game.gd
+++ game.gd
@@ -7,10 +7,14 @@ const ASTEROID_POINTS = 1
 
 var score = 0
 
+func _ready():
+	set_score()
+
 func spawn_asteroid():
 	var asteroid = preload("res://asteroid.tscn").instantiate()
 	%PathFollow2D.progress_ratio = randf()
 	asteroid.global_position = %PathFollow2D.global_position
+	asteroid.disappeared.connect(_on_asteroid_disappeared)
 	asteroid.destroyed.connect(_on_asteroid_destroyed)
 	add_child(asteroid)
 	
@@ -22,6 +26,13 @@ func _on_timer_timeout():
 		spawn_asteroid()
 		current_asteroid_count += 1
 
+func _on_asteroid_disappeared():
+	current_asteroid_count -= 1
 
 func _on_asteroid_destroyed():
 	current_asteroid_count -= 1
+	score += ASTEROID_POINTS
+	set_score()
+
+func set_score():
+	%Score.text = "Score: %s" % score
