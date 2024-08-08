8125f5cf566aa0831444998ad228e6a45130a7b0
Jacob Howell <jhowell45@googlemail.com>
2024-08-08 13:32:19 +0100

    connect the dead asteroid signal to update the current count.

--- game.gd
+++ game.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
-var asteroid_limit = 10
+var asteroid_limit = 200
 var current_asteroid_count = 0
 
 func spawn_asteroid():
 	var asteroid = preload("res://asteroid.tscn").instantiate()
 	%PathFollow2D.progress_ratio = randf()
 	asteroid.global_position = %PathFollow2D.global_position
+	asteroid.destroyed.connect(_on_asteroid_destroyed)
 	add_child(asteroid)
 	
 
@@ -15,3 +16,7 @@ func _on_timer_timeout():
 	if current_asteroid_count < asteroid_limit:
 		spawn_asteroid()
 		current_asteroid_count += 1
+
+
+func _on_asteroid_destroyed():
+	current_asteroid_count -= 1
